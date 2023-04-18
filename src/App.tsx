1a4b22d6e4e20af28ddcd55abf7b94610181aac5
Erkang Xia <xiaerkang@icloud.com>
2023-04-18 11:04:56 -0400

    finish chart

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
+
 import { Line } from "react-chartjs-2";
 import type { ChartData, ChartOptions } from "chart.js";
 // Register the required components
@@ -31,11 +32,14 @@ ChartJS.register(
 function App() {
   const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
   const [selected, setSelected] = useState<Crypto | null>();
+  const [range, setRange] = useState<string>("30");
+
   const [data, setData] = useState<ChartData<"line">>();
   const [options, setOptions] = useState<ChartOptions<"line">>({
     responsive: true,
     plugins: {
       legend: {
+        display: false,
         position: "top" as const,
       },
       title: {
@@ -54,6 +58,55 @@ function App() {
     });
   }, []);
 
+  useEffect(() => {
+    if (!selected) return;
+    axios
+      .get(
+        `https://api.coingecko.com/api/v3/coins/${
+          selected?.id
+        }/market_chart?vs_currency=usd&days=${range}&${
+          range === "1" ? "interval=hourly" : "interval=daily"
+        }`
+      )
+      .then((response) => {
+        console.log(response.data);
+        setData({
+          labels: response.data.prices.map((price: number[]) => {
+            if (range === "1") {
+              return moment.unix(price[0] / 1000).format("HH:MM");
+            }
+            return moment.unix(price[0] / 1000).format("MM-DD-YYYY");
+          }),
+          datasets: [
+            {
+              label: "Dataset 1",
+              data: response.data.prices.map((price: number[]) => {
+                return price[1];
+              }),
+              borderColor: "rgb(255, 99, 132)",
+              backgroundColor: "rgba(255, 99, 132, 0.5)",
+            },
+          ],
+        });
+
+        setOptions({
+          responsive: true,
+          plugins: {
+            legend: {
+              display: false,
+              position: "top" as const,
+            },
+            title: {
+              display: true,
+              text:
+                `${selected?.name} Price over last ` +
+                (range === "1" ? "24 Hours " : range + "days "),
+            },
+          },
+        });
+      });
+  }, [selected, range]);
+
   return (
     <>
       <div className="App">
@@ -64,28 +117,6 @@ function App() {
             });
 
             setSelected(selec);
-            axios
-              .get(
-                `https://api.coingecko.com/api/v3/coins/${selec?.id}/market_chart?vs_currency=usd&days=30&interval=daily`
-              )
-              .then((response) => {
-                console.log(response.data);
-                setData({
-                  labels: response.data.prices.map((price: number[]) => {
-                    return moment.unix(price[0] / 1000).format("MM-DD-YYYY");
-                  }),
-                  datasets: [
-                    {
-                      label: "Dataset 1",
-                      data: response.data.prices.map((price: number[]) => {
-                        return price[1];
-                      }),
-                      borderColor: "rgb(255, 99, 132)",
-                      backgroundColor: "rgba(255, 99, 132, 0.5)",
-                    },
-                  ],
-                });
-              });
           }}
           defaultValue={"default"}
         >
@@ -105,6 +136,16 @@ function App() {
                 );
               })
             : null}
+        </select>
+
+        <select
+          onChange={(e) => {
+            setRange(e.target.value);
+          }}
+        >
+          <option value="30">30 Days</option>
+          <option value="7">7 Days</option>
+          <option value="1">1 Days</option>
         </select>
       </div>
       {selected ? <CryptoSummary crypto={selected} /> : null}
